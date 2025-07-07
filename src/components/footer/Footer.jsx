b0f346e1cb3010d67f79d4a6490ae5fb1dd2b2c3
Ansarshahzadi <ansarilyas.itroots@gmail.com>
2025-07-07 21:15:03 +0500

    deleting unnecessary things

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -59,29 +59,12 @@ const Footer = () => {
               </div>
             </div>
             <div className="col-md-6  text-dark fw-bold mb-3 px-5">
-              {/* <h3 data-aos="fade-up" className="fw-bold  text-dark mb-3">
-                Subscribe with name
-              </h3>
-              <input
-                data-aos="fade-up"
-                type="text"
-                placeholder="Your name"
-                className="form-control rounded-0 mb-1 w-75 p-3 border-0 shadow"
-              />
-              <a href="" className="text-decoration-none">
-                <div
-                  data-aos="fade-up"
-                  className="btn btn-primary my-3 py-2 px-4  text-white"
-                >
-                  Subscribe
-                </div>
-              </a> */}
               <div className="container text-dark  bg-body-tertiary  mb-3">
                 <div className="row " data-aos="fade-up">
                   <div className="col-md-12 text-start">
                     <a href="" className="text-decoration-none text-dark fw-bold">
                       <img src={icon1} alt="" className="me-4 p-2 mb-3  bg-primary rounded" />
-                      Call: +923287315641
+                      Call: +92 328 7315641
                     </a>
                   </div>
                   <div className="col-md-12 text-start">
